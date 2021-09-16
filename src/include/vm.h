43f3173defa3e84008ef68c21e22dd205d11a092
brkp <brkp@tutanota.com>
2021-09-16 22:23:12 +0300

    vm implementation

--- src/include/vm.h
+++ src/include/vm.h
@@ -1,4 +1,17 @@
 #ifndef bf_vm_h
 #define bf_vm_h
 
+#include "array.h"
+#include "opcode.h"
+
+typedef struct virtual_machine {
+    int ip;
+    int dp;
+    array(unsigned char, mem);
+} VM;
+
+VM   *vm_new(int size);
+int   vm_execute(VM *vm, OpCodeArray *program);
+void  vm_free(VM *vm);
+
 #endif
--- src/modules/vm.c
+++ src/modules/vm.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "vm.h"
+#include "array.h"
+#include "opcode.h"
+
+VM *vm_new(int size) {
+    VM *vm = (VM *) malloc(sizeof(VM));
+
+    vm->ip = 0;
+    vm->dp = 0;
+    array_init(unsigned char, &vm->mem, size);
+
+    return vm;
+}
+
+void vm_free(VM *vm) {
+    array_free(&vm->mem);
+    free(vm);
+}
+
+int vm_execute(VM *vm, OpCodeArray *program) {
+    for (;;) {
+        OpCode instruction = program->values[vm->ip];
+
+        switch (instruction.type) {
+            case OP_PCH:
+                putchar(vm->mem.values[vm->dp]);
+                break;
+            case OP_GCH:
+                vm->mem.values[vm->dp] = getchar();
+                break;
+            case OP_ADD:
+                vm->mem.values[vm->dp] += instruction.value;
+                break;
+            case OP_INC:
+                vm->dp += instruction.value;
+                break;
+            case OP_JZE:
+                vm->ip = vm->mem.values[vm->dp] == 0 ? instruction.value : vm->ip + 1;
+                continue;
+            case OP_JNZ:
+                vm->ip = vm->mem.values[vm->dp] != 0 ? instruction.value : vm->ip + 1;
+                continue;
+            case OP_HALT:
+                return 0;
+        }
+
+        vm->ip++;
+    }
+}
