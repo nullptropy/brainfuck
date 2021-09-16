b8321b221373df7784ac0323602149095b72b8e1
brkp <brkp@tutanota.com>
2021-09-16 14:51:52 +0300

    entry point

--- src/main.c
+++ src/main.c
@@ -5,6 +5,63 @@
 #include "opcode.h"
 #include "compiler.h"
 
+void repl();
+void run_file(const char *path);
+static char *read_file(const char *path);
+
 int main(int argc, char **argv) {
+    switch (argc) {
+        case 1: repl(); break;
+        case 2: run_file(argv[1]); break;
+        default:
+            fprintf(stderr, "usage: %s [path]\n", argv[0]);
+            break;
+    }
+
     return 0;
+}
+
+void repl() {}
+
+void run_file(const char *path) {
+    char *source = read_file(path);
+    OpCodeArray program = compile(source);
+    free(source);
+
+    printf("[");
+    for (int i = 0; i < program.num; i++) {
+        opcode_print(program.values[i]); printf(", ");
+        free(program.values[i]);
+    }
+    printf("\b\b]\n");
+
+    array_free(&program);
+}
+
+static char *read_file(const char *path) {
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "failed to open file %s\n", path);
+        exit(1);
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t file_size = ftell(file);
+    rewind(file);
+
+    char *buffer = (char*) malloc(file_size + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "failed to allocate memory to read %s\n", path);
+        exit(1);
+    }
+
+    size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+    if (bytes_read < file_size) {
+        fprintf(stderr, "failed to read the file %s\n", path);
+        exit(1);
+    }
+    buffer[file_size] = '\0';
+
+    fclose(file);
+    return buffer;
 }
