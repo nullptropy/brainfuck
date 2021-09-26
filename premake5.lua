803a71fda39576833f72d40d4f82d5bfd6c6acf1
brkp <brkp@tutanota.com>
2021-09-26 15:55:17 +0300

    remove readline dependency

--- premake5.lua
+++ premake5.lua
@@ -10,8 +10,6 @@ project "bfc"
     files { "**.h", "**.c" }
     includedirs { "src/include" }
 
-    links { "readline" }
-
     filter "configurations:debug"
         defines { "DEBUG" }
         symbols "On"
